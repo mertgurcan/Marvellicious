2a800f4a7136d19d796c5fff80810585be0e8b8d
Mert Gurcan <mertgurcan@alumni.bilkent.edu.tr>
2022-02-20 14:59:45 +0300

    Initial Commit

---
+++ Marvellicious/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvellicious
+//
+//  Created by Mert Gürcan on 20.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
